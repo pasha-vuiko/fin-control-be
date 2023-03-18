f772fe271d9c125f1acdb6275b899c26ec7df165
pavlo-vuiko <pasha.vuiko@gmail.com>
2023-03-18 13:35:42 +0200

    fix(shared): fix PaginationDto validation

--- src/shared/dto/pagination.dto.ts
+++ src/shared/dto/pagination.dto.ts
@@ -1,14 +1,17 @@
 import { NotRequired } from '@shared/decorators/validation/not-required.decorator';
-import { IsNumberString } from 'class-validator';
+import { IsNumber } from 'class-validator';
 import { IPagination } from '@shared/interfaces/pagination.interface';
 import { DEFAULT_PAGINATION } from '@shared/constants/pagination-defaults';
+import { Transform } from 'class-transformer';
 
 export class PaginationDto implements IPagination {
-  @IsNumberString()
+  @IsNumber()
   @NotRequired()
+  @Transform(({ value }) => Number(value))
   skip?: number = DEFAULT_PAGINATION.skip;
 
-  @IsNumberString()
+  @IsNumber()
   @NotRequired()
+  @Transform(({ value }) => Number(value))
   take?: number = DEFAULT_PAGINATION.take;
 }
