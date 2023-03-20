a585b835915fa3e23f05bcb34a9e9cbb49fb7dc0
pavlo-vuiko <pasha.vuiko@gmail.com>
2023-03-20 14:42:38 +0200

    test(api): fix hanging of unit tests

--- src/api/expenses/expenses.service.spec.ts
+++ src/api/expenses/expenses.service.spec.ts
@@ -3,11 +3,17 @@ import { ExpensesService } from './expenses.service';
 import { PrismaModule } from '@shared/modules/prisma/prisma.module';
 import { CustomersModule } from '@api/customers/customers.module';
 import { ExpensesRepository } from '@api/expenses/repositories/expenses.repository';
+import { RedisConfigService } from '@shared/modules/redis/services/redis-config/redis-config.service';
+import { IoredisWithDefaultTtl } from '@shared/modules/redis/classes/ioredis-with-default-ttl';
 
 describe('ExpensesService', () => {
   let service: ExpensesService;
 
   beforeEach(async () => {
+    jest
+      .spyOn(RedisConfigService, 'getIoRedisInstance')
+      .mockReturnValue({} as IoredisWithDefaultTtl);
+
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule, CustomersModule],
       providers: [ExpensesService, ExpensesRepository],
