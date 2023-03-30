16b60ee5a2ac8fb61132a1ed5b09cb71604fd957
pavlo-vuiko <pasha.vuiko@gmail.com>
2023-03-30 23:58:07 +0300

    test(app): fix test suite to support Fastify

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,19 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { packageJsonInfo } from '@shared/constants/package-json-info';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { config } from '../src/app.config';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(config.app.fastify),
+    );
+
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
   });
 
   afterEach(async () => {
