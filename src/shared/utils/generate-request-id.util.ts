7620d71155404c8b1209854b4229ece8f80a054d
pavlo-vuiko <pasha.vuiko@gmail.com>
2023-04-01 17:43:58 +0300

    feat(app): add new expense types

--- src/shared/utils/generate-request-id.util.ts
+++ src/shared/utils/generate-request-id.util.ts
@@ -3,6 +3,7 @@ import { randomUUID } from 'node:crypto';
 import { REQUEST_ID_HEADER } from '@shared/modules/logger/constants/request-id-header';
 
 export function generateRequestId(req: FastifyRequest): string {
+  // eslint-disable-next-line security/detect-object-injection
   const existingTraceId = req.headers[REQUEST_ID_HEADER];
 
   if (existingTraceId) {
