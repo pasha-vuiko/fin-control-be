d7965b9fcdf43aadcacba4a395e9d82961c94c03
pavlo-vuiko <pasha.vuiko@gmail.com>
2023-04-01 17:43:58 +0300

    feat(app): add new expense types

---
+++ prisma/migrations/20230401144212_add_new_expense_types/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - The values [HOBBIES,GAMES] on the enum `ExpenseType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+ALTER TYPE "ExpenseType"ADD VALUE 'MEDECINE';
+ALTER TYPE "ExpenseType"ADD VALUE 'UTILITY_PAYMENTS';
+ALTER TYPE "ExpenseType"ADD VALUE 'ANIMALS';
+ALTER TYPE "ExpenseType"ADD VALUE 'PLACES_TO_EAT';
+ALTER TYPE "ExpenseType"ADD VALUE 'EDUCATION';
+ALTER TYPE "ExpenseType"ADD VALUE 'BOOKS';
+ALTER TYPE "ExpenseType"ADD VALUE 'TAXI';
+ALTER TYPE "ExpenseType"ADD VALUE 'GIFTS';
+ALTER TYPE "ExpenseType"ADD VALUE 'DONATIONS';
+ALTER TYPE "ExpenseType"ADD VALUE 'MOBILE_SERVICES';
+ALTER TYPE "ExpenseType"ADD VALUE 'SPORTS';
+ALTER TYPE "ExpenseType"ADD VALUE 'ENTERAINMENT';
+ALTER TYPE "ExpenseType"ADD VALUE 'BEAUTY_AND_CARE';
+ALTER TYPE "ExpenseType"ADD VALUE 'HOUSEHOLD';
+ALTER TYPE "ExpenseType"ADD VALUE 'PUBLIC_TRANSPORT';
+ALTER TYPE "ExpenseType"ADD VALUE 'TRAVEL';
+ALTER TYPE "ExpenseType"DROP VALUE 'HOBBIES';
+ALTER TYPE "ExpenseType"DROP VALUE 'GAMES';
