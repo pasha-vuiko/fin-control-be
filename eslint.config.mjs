267a7f9af1f253877fea6e79e232b5ce6ff12bab
pavlo-vuiko <pasha.vuiko@gmail.com>
2024-11-28 23:30:23 +0200

    fix(shared): add patch to drizzle-prisma-generator to make native type mappings more accurate

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,8 @@ export default [
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-empty-interface': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/only-throw-error': 'error',
+      '@typescript-eslint/no-shadow': 'warn',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
