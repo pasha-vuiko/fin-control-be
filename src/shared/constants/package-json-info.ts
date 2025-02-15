1f6d87ee13cfc5ed5e7772cb816f301bac3a2ff9
pavlo-vuiko <pasha.vuiko@gmail.com>
2025-02-15 14:04:40 +0200

    test(app): fix unit tests

--- src/shared/constants/package-json-info.ts
+++ src/shared/constants/package-json-info.ts
@@ -1,10 +1,11 @@
 import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
+import { findPackageJSON } from 'node:module';
 
 import { IPackageJson } from '@shared/interfaces/package-json.interface';
 
-const packageJsonPath = resolve(__dirname, '..', '..', '..', '..', 'package.json');
-const packageJsonContentStr = readFileSync(packageJsonPath, 'utf-8');
+const packageJsonPath = findPackageJSON('..', __filename);
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+const packageJsonContentStr = readFileSync(packageJsonPath!, 'utf-8');
 
 // Content of package.json
 export const packageJsonInfo: IPackageJson = JSON.parse(packageJsonContentStr);
